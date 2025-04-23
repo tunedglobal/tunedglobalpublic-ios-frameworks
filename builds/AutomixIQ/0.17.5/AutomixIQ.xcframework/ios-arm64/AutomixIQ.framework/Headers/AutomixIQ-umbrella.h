a10c30e6c08008267ad6ac3eea1fed62260d8d1d
Pacemaker <pacemaker@Vics-MacBook-Pro.local>
2025-04-23 15:49:11 +0200

    AutomixIQ -> 0.17.5

---
+++ builds/AutomixIQ/0.17.5/AutomixIQ.xcframework/ios-arm64/AutomixIQ.framework/Headers/AutomixIQ-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "automix-sdk/AutomixIQ.h"
+#import "automix-sdk/CoreDataModels/_AutomixIQPlayLog.h"
+#import "automix-sdk/CoreDataModels/AutomixIQPlayLog.h"
+#import "automix-sdk/CoreDataModels/_AutomixIQTransitionLog.h"
+#import "automix-sdk/CoreDataModels/AutomixIQTransitionLog.h"
+#import "automix-sdk/MDMCoreDataWrapper.h"
+
+FOUNDATION_EXPORT double AutomixIQVersionNumber;
+FOUNDATION_EXPORT const unsigned char AutomixIQVersionString[];
