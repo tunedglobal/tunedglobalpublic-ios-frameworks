29f630946cd1c6069edf7cf7a66ad597208846df
Victor Garcia <victor.garcia@tunedglobal.com>
2026-08-10 18:11:17 +0200

    SocialRadio -> 0.22.6

---
+++ builds/SocialRadio/0.22.6/SocialRadio.xcframework/ios-arm64_x86_64-simulator/SocialRadio.framework/Headers/automix-sdk/CoreDataModels/_AutomixIQTransitionLog.h
@@ -0,0 +1,102 @@
+// DO NOT EDIT. This file is machine-generated and constantly overwritten.
+// Make changes to AutomixIQPlayLog.h instead.
+
+#if __has_feature(modules)
+    @import Foundation;
+    @import CoreData;
+#else
+    #import <Foundation/Foundation.h>
+    #import <CoreData/CoreData.h>
+#endif
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AutomixIQTransitionLogID : NSManagedObjectID {}
+@end
+
+@interface _AutomixIQTransitionLog : NSManagedObject
++ (instancetype)insertInManagedObjectContext:(NSManagedObjectContext *)moc_;
++ (NSString*)entityName;
++ (nullable NSEntityDescription*)entityInManagedObjectContext:(NSManagedObjectContext*)moc_;
+@property (nonatomic, readonly, strong) AutomixIQTransitionLogID *objectID;
+
+@property (nonatomic, strong, nullable) NSString* guid;
+
+@property (nonatomic, strong) NSString* transitionType;
+
+@property (nonatomic, strong) NSNumber* duration;
+
+@property (atomic) int64_t durationValue;
+- (int64_t)durationValue;
+- (void)setDurationValue:(int64_t)value_;
+
+@property (nonatomic, strong) NSNumber* trackIdOut;
+
+@property (atomic) int64_t trackIdOutValue;
+- (int64_t)trackIdOutValue;
+- (void)setTrackIdOutValue:(int64_t)value_;
+
+@property (nonatomic, strong) NSNumber* trackIdIn;
+
+@property (atomic) int64_t trackIdInValue;
+- (int64_t)trackIdInValue;
+- (void)setTrackIdInValue:(int64_t)value_;
+
+@property (nonatomic, strong) NSNumber* userId;
+
+@property (atomic) int64_t userIdValue;
+- (int64_t)userIdValue;
+- (void)setUserIdValue:(int64_t)value_;
+
+@property (nonatomic, strong) NSDate* when;
+
+@end
+
+@interface _AutomixIQTransitionLog (CoreDataGeneratedPrimitiveAccessors)
+
+- (nullable NSString*)primitiveGuid;
+- (void)setPrimitiveGuid:(nullable NSString*)value;
+
+- (NSString*)primitiveTransitionType;
+- (void)setPrimitiveTransitionType:(NSString*)value;
+
+- (NSNumber*)primitiveDuration;
+- (void)setPrimitiveDuration:(NSNumber*)value;
+
+- (int64_t)primitiveDurationValue;
+- (void)setPrimitiveDurationValue:(int64_t)value_;
+
+- (NSNumber*)primitiveTrackIdOut;
+- (void)setPrimitiveTrackIdOut:(NSNumber*)value;
+
+- (int64_t)primitiveTrackIdOutValue;
+- (void)setPrimitiveTrackIdOutValue:(int64_t)value_;
+
+- (NSNumber*)primitiveTrackIdIn;
+- (void)setPrimitiveTrackIdIn:(NSNumber*)value;
+
+- (int64_t)primitiveTrackIdInValue;
+- (void)setPrimitiveTrackIdInValue:(int64_t)value_;
+
+- (NSNumber*)primitiveUserId;
+- (void)setPrimitiveUserId:(NSNumber*)value;
+
+- (int64_t)primitiveUserIdValue;
+- (void)setPrimitiveUserIdValue:(int64_t)value_;
+
+- (NSDate*)primitiveWhen;
+- (void)setPrimitiveWhen:(NSDate*)value;
+
+@end
+
+@interface AutomixIQTransitionLogAttributes: NSObject
++ (NSString *)guid;
++ (NSString *)transitionType;
++ (NSString *)duration;
++ (NSString *)trackIdOut;
++ (NSString *)trackIdIn;
++ (NSString *)userId;
++ (NSString *)when;
+@end
+
+NS_ASSUME_NONNULL_END
